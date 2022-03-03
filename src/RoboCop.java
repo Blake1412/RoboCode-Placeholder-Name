6424e65ff790e3ede428e829c85f58cc3bdb680c
Blake Ryan <blakeryan30@gmail.com>
2022-03-03 16:41:47 +0000

    Default robot code for basic functionality

--- src/RoboCop.java
+++ src/RoboCop.java
@@ -1,5 +1,27 @@
 package ul;
 
+import robocode.*;
+import java.awt.Color;
+
 public class RoboCop extends Robot {
-    
+    public void run() {
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+        back(10);
+    }
+
+    public void onHitWall(HitWallEvent e) {
+        back(20);
+    }
 }
