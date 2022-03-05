310c4ec5e01e7e802aa59696fa3c99b23ffa8507
Italo Terto <italoterto4@gmail.com>
2022-03-05 16:49:38 +0000

    A few changes made by Milan and I

--- src/RoboCop.java
+++ src/RoboCop.java
@@ -4,24 +4,52 @@ import robocode.*;
 import java.awt.Color;
 
 public class RoboCop extends Robot {
+    int arenaHeight;
+	int arenaWidth;
+
     public void run() {
         while (true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+            turnRight(45);
         }
     }
 
+    public void movement() {
+        ahead(100);
+		turnRight(90);
+    }
+
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+        fire(3);
     }
 
     public void onHitByBullet(HitByBulletEvent e) {
-        back(10);
+        turnRight(e.getBearing());
     }
 
     public void onHitWall(HitWallEvent e) {
-        back(20);
+        double loc;
+		loc=e.getBearing();
+		
+		if(loc>0){
+			turnLeft(90);
+		} else {
+			turnRight(90);
+		}
+
+		ahead(100);
     }
+
+    public void checkLocation(){
+		double value = getX();
+	}
+
+    public void smartFire(double robotDistance) {
+		if (robotDistance > 400) {
+			fire(1);
+		} else if (robotDistance > 200) {
+			fire(2);
+		} else {
+			fire(3);
+		}
+	}
 }
