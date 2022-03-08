68aad8a9a7cf05f0ac997ef25d13d627208b71c0
Blake1412 <blakeryan30@gmail.com>
2022-03-08 17:30:05 +0000

    Predictive targetting system, and general finishing touches to the code

--- src/RoboCop.java
+++ src/RoboCop.java
@@ -1,78 +1,147 @@
 package ul;
 
 import robocode.*;
-import java.awt.Color;
+import robocode.util.*;
 
+import java.util.Arrays;
 
 public class RoboCop extends Robot {
-    int arenaHeight;
-    int arenaWidth;
-    
     public void run() {
+        setAdjustRadarForGunTurn(true);
+        setAdjustRadarForRobotTurn(true);
+        setAdjustGunForRobotTurn(true);
+
         turnRight(turnUpright());
-        toCenter(getQuadrant());        
+        toCenter(getQuadrant());
         turnRight(turnUpright());
         turnLeft(45);
 
-        while(true) {
-            movement();
+        while (true) {
+            turnRadarRight(Double.POSITIVE_INFINITY);
         }
-    }
-
-    public void movement() {
-        ahead(100);
-        turnRight(90);
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        double robotDistance = e.getDistance();
-        
-        if (robotDistance > 400) {
-            fire(1);
-        } else if (robotDistance > 200) {
-            fire(2);
-        } else {
-            fire(3);
-        }
+        engageTarget(e);
     }
-    
-    public void onHitByBullet(HitByBulletEvent e) {
-        turnRight(e.getBearing());
+
+    public void engageTarget(ScannedRobotEvent e) {
+        double distance = e.getDistance();
+        int firepower = 0;
+
+        if (distance >= 500)
+            firepower = 1;
+        else if (distance >= 300)
+            firepower = 2;
+        else
+            firepower = 3;
+
+        double absoluteBearing = (Math.toRadians(getHeading())) + e.getBearingRadians();
+        double enemyX = getX() + e.getDistance() * Math.sin(absoluteBearing);
+        double enemyY = getY() + e.getDistance() * Math.cos(absoluteBearing);
+
+        double enemyHeading = 0, enemyHeadingOld = 0, enemyHeadingChange = 0;
+        enemyHeadingOld = enemyHeading;
+        enemyHeading = e.getHeadingRadians();
+        enemyHeadingChange = enemyHeading - enemyHeadingOld;
+        double velocity = e.getVelocity();
+
+        double deltaTime = 0;
+        double predictedX = enemyX, predictedY = enemyY;
+        while (deltaTime * (20 - 3 * firepower) < distanceBetweenPoints(getX(), getY(), predictedY, predictedY)) {
+            deltaTime++;
+            predictedX += Math.sin(enemyHeading) * velocity;
+            predictedY += Math.cos(enemyHeading) * velocity;
+            enemyHeading += enemyHeadingChange;
+        }
+
+        double angle = Utils.normalAbsoluteAngle(Math.atan2(predictedX - getX(), predictedY - getY()));
+        rotateGun(predictedX, predictedY);
+        fire(firepower);
+        movement();
+    }
+
+    public double distanceBetweenPoints(double x1, double x2, double y1, double y2) {
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
+
+    public void rotateGun(double enemyX, double enemyY) {
+        double angle = absoluteBearing(getX(), getY(), enemyX, enemyY);
+        turnGunRight(normalizedBearing(angle - getGunHeading()));
+    }
+
+    double absoluteBearing(double x1, double y1, double x2, double y2) {
+        double xo = x2 - x1;
+        double yo = y2 - y1;
+        double hyp = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        double arcSin = Math.toDegrees(Math.asin(xo / hyp));
+        double bearing = 0;
+
+        if (xo > 0 && yo > 0) { // both pos: lower-Left
+            bearing = arcSin;
+        } else if (xo < 0 && yo > 0) { // x neg, y pos: lower-right
+            bearing = 360 + arcSin; // arcsin is negative here, actuall 360 - ang
+        } else if (xo > 0 && yo < 0) { // x pos, y neg: upper-left
+            bearing = 180 - arcSin;
+        } else if (xo < 0 && yo < 0) { // both neg: upper-right
+            bearing = 180 - arcSin; // arcsin is negative here, actually 180 + ang
+        }
+
+        return bearing;
+    }
+
+    double normalizedBearing(double angle) {
+        while (angle > 180)
+            angle -= 360;
+        while (angle < -180)
+            angle += 360;
+        return angle;
+    }
+
+    public void movement() {
+        ahead(75);
+        turnRight(90);
     }
 
     public void onHitWall(HitWallEvent e) {
+
         double loc;
-        loc=e.getBearing();
-        
-        if(loc>0){
+        loc = e.getBearing();
+
+        if (loc > 0) {
             turnLeft(90);
-        } else {
+        } // close if
+        else {
             turnRight(90);
-        }
+        } // close else
         ahead(100);
-    }
-        
-    public void checkLocation(){
+    }// closes onHitWall
+
+    public void checkLocation() {
         double value = getX();
     }
-    
-    public double turnUpright(){
+
+    /**
+     * This method is very verbose to explain how things work.
+     * Do not obfuscate/optimize this sample.
+     */
+    public double turnUpright() {
         double head = getHeading();
         return (360 - head);
     }
 
-    public int getQuadrant(){
+    public int getQuadrant() {
         double x = getX();
         double y = getY();
         int store = 0;
-        if(x <= 400 && y <= 400){
-            store =  1;
-        } else if(x <= 400 && y <= 800) {
-            store =  2;
-        } else if(x <= 800 && y <= 400) {
-            store =  3;
+        if (x <= 400 && y <= 400) {
+            store = 1;
+        } else if (x <= 400 && y <= 800) {
+            store = 2;
+        } else if (x <= 800 && y <= 400) {
+            store = 3;
         } else {
-            store =  4;
+            store = 4;
         }
         System.out.println("THE QUADRANT: " + store);
         return store;
@@ -82,19 +151,19 @@ public class RoboCop extends Robot {
         double x, y;
         x = getX();
         y = getY();
-        
-        if(quadrant == 1 || quadrant == 2) {
+
+        if (quadrant == 1 || quadrant == 2) {
             turnRight(90);
         } else {
             turnLeft(90);
         }
         ahead(Math.abs(400 - x));
-
-        if(quadrant == 1 || quadrant == 4) {
+        if (quadrant == 1 || quadrant == 4) {
             turnLeft(90);
         } else {
             turnRight(90);
         }
+
         ahead(Math.abs(400 - y));
     }
 }
